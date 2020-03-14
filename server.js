8a5f2b2d7815e9c29078c968a697866165442956
Marcin Kubiak <hugo26@o2.pl>
2020-03-14 22:07:52 +0100

    add form submit fuctionality

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('express-handlebars');
+const multer = require('multer');
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function(req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage: storage });
 
 const app = express();
 app.engine('hbs', hbs());
 app.set('view engine', 'hbs');
 
 app.use(express.static(path.join(__dirname + '/public')));
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.render('index');
@@ -22,6 +33,21 @@ app.get('/about', (req, res) => {
 
 app.get('/contact', (req, res) => {
   res.render('contact');
+});
+
+app.post('/contact/send-message', upload.single('file'), (req, res) => {
+  const { file } = req;
+  const { author, sender, title, message } = req.body;
+
+  if (author && sender && title && message && file) {
+    res.render('contact', {
+      isSent: true,
+      filename: file.originalname,
+      filepath: file.path,
+    });
+  } else {
+    res.render('contact', { isError: true });
+  }
 });
 
 app.get('/info', (req, res) => {
